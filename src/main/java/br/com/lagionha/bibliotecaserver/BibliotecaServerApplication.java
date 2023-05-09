4c1a5824954813f86e84b2e3c6dee4fa4f9c1830
Renato Aires <rentonaires@gmail.com>
2023-05-09 13:53:24 -0300

    first commit

---
+++ src/main/java/br/com/lagionha/bibliotecaserver/BibliotecaServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.lagionha.bibliotecaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaServerApplication.class, args);
+	}
+
+}
